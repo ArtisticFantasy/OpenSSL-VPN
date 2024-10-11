2786bc92e2bb4b6159dd39fd6f182d9953381ccf
Yizhou Zhao <1694888082@qq.com>
2024-10-11 16:52:02 +0800

    mkdir certs

--- gen_certs.sh
+++ gen_certs.sh
@@ -3,6 +3,8 @@ PROJ_DIR=$(realpath $(dirname $0))
 OPENSSL=$PROJ_DIR/tools/openssl/bin/openssl
 OPENSSL_LIB=$PROJ_DIR/tools/openssl/lib64:$PROJ_DIR/tools/openssl/lib
 
+mkdir -p $PROJ_DIR/certs
+
 yes "" | LD_LIBRARY_PATH=$OPENSSL_LIB $OPENSSL genpkey -algorithm RSA -out $PROJ_DIR/certs/host.key -pkeyopt rsa_keygen_bits:2048
 yes "" | LD_LIBRARY_PATH=$OPENSSL_LIB $OPENSSL req -new -key $PROJ_DIR/certs/host.key -out $PROJ_DIR/certs/host.csr
 yes "" | LD_LIBRARY_PATH=$OPENSSL_LIB $OPENSSL x509 -req -days 365 -in $PROJ_DIR/certs/host.csr -signkey $PROJ_DIR/certs/host.key -out $PROJ_DIR/certs/host.crt
